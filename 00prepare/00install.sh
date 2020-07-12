148e396a973a6c50983c30627e8bc042da47626e
Andy Oakley <andy@andyoakley.com>
2020-07-12 15:52:06 -0700

    First steps

---
+++ 00prepare/00install.sh
@@ -0,0 +1,26 @@
+# setup Docker
+apt-get install docker.io 
+
+# Set up the Docker daemon
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+systemctl enable docker
+
+# networking for Docker
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+# needed for OpenEBS later
+apt-get install open-iscsi
